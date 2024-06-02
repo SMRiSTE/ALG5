fcd0df66d4efbb737f6e537d7fa256dafbf84e11
SMRist <s.m.r.00800ist@gmail.com>
2024-06-02 11:43:51 +0300

    Initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int simple_string_hash(std::string& str) {
+    int hash = 0;
+    for (char c : str) {
+        hash += static_cast<int>(c);
+    }
+    return hash;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    std::string input;
+    std::cout << "Введите строку: ";
+    std::cin >> input;
+
+    while (true) {
+        int hash = simple_string_hash(input);
+        std::cout << "Наивный хэш строки " << input << " = " << hash << std::endl;
+        std::cout << "Введите строку: ";
+        std::cin >> input;
+    }
+
+    return 0;
+}
---
+++ 2/2/2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include<cmath>
+
+int real_string_hash(int p, int n, std::string& str) {
+    int hash = 0; 
+    for (int i = 0; i < str.length(); i++) {
+        hash += (static_cast<char>(str[i]) * pow(p, i));
+    }
+    return hash % n;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    std::string input;
+    int p = 0;
+    int n = 0;
+
+    std::cout << "Введите p: ";
+    std::cin >> p;
+    std::cout << "Введите n: ";
+    std::cin >> n;
+    std::cout << "Введите строку: ";
+    std::cin >> input;
+
+    while (true) {
+        int hash = real_string_hash(p, n, input);
+        std::cout << "Хэш строки " << input << " = " << hash << std::endl;
+        if (input == "exit") {
+            break;
+        }
+        std::cout << "Введите строку: ";
+        std::cin >> input;
+    }
+
+    return 0;
+}
